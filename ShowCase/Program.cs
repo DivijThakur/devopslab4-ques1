adfe129634a96f0ecd6d4d9ac87a2dc734c5d766
divij singh <divijsingh2@gmail.com>
2023-04-14 15:42:50 -0400

    adding string project

---
+++ ShowCase/Program.cs
@@ -0,0 +1,37 @@
+﻿using UtilityLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int row = 0;
+
+        do
+        {
+            if (row == 0 || row >= 20)
+                ResetConsole();
+
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) break;
+            Console.WriteLine($"Input: {input}");
+            Console.WriteLine("Does it start with uppercase letter? " +
+                 $"{(input.StartsWithUpper() ? "Yes" : "No")}");
+
+            Console.WriteLine();
+            row += 4;
+        } while (true);
+        return;
+
+        void ResetConsole()
+        {
+            if (row > 0)
+            {
+             
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine($"{Environment.NewLine}Press enter to continue:{Environment.NewLine}");
+            row = 3;
+        }
+    }
+}
